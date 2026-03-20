76c4ec94658be23822e1b4b497c32e015b422eb3
naikkirti62-crypto <naikkirti62@gmail.com>
2026-03-20 13:08:54 +0530

    Update coin.js

--- coin.js
+++ coin.js
@@ -1,9 +1,8 @@
 function coinChange(coins, amount) {
-    // Create dp array filled with Infinity, dp[0] = 0
+    
     let dp = new Array(amount + 1).fill(Infinity);
     dp[0] = 0;
 
-    // For each amount from 1 to target
     for (let i = 1; i <= amount; i++) {
         for (let coin of coins) {
             if (coin <= i) {
